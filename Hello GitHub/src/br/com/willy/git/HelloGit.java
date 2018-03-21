4fab3a8c83d026bef86b1209fe313ea589b0974f
Williperes <37600079+Williperes@users.noreply.github.com>
2018-03-20 21:57:01 -0300

    adicionei arquivos via upload

---
+++ Hello GitHub/src/br/com/willy/git/HelloGit.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package br.com.willy.git;
+
+/**
+ * @author wilibaldo.lperes1
+ * 
+ */
+public class HelloGit {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello GitHub");
+
+	}
+
+}
